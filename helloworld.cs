c990082b49ec8e919d202d7483c37346a822c372
Dominique Burke <Dominique.Burke@footballpools.com>
2021-05-13 16:58:05 +0100

    added hellworld file

---
+++ helloworld.cs
@@ -0,0 +1,11 @@
+// Hello World! program
+namespace helloworld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
